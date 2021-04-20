346d57374c2f4c6ae39e8a32969ef014180c08be
Lee Hopper <hopper.lee.j@gmail.com>
2021-04-19 20:23:46 -0600

    Add model for animal using hash syntax

--- hash-examples.rb
+++ hash-examples.rb
@@ -1,7 +1,14 @@
 
 car = {
-  "color" => "black"
-  "make" => "jeep"
-  "model" => "grand cherokee"
-  "miles" => 190000
+  "color" => "black",
+  "make" => "jeep",
+  "model" => "grand cherokee",
+  "miles" => 190000,
+}
+
+animal = {
+  color: "black tri",
+  age: 1.5,
+  weight: 150,
+  name: "Alfie"
 }
